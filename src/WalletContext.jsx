fb687b1e3d95fa0e014579bfbe8130a9f7e3d98a
Vortex Dapp <megabobbyinu@gmail.com>
2025-01-03 19:03:31 +0000

    Update WalletContext.jsx

--- src/WalletContext.jsx
+++ src/WalletContext.jsx
@@ -1,17 +1,13 @@
-import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
-import '@solana/wallet-adapter-react-ui/styles.css';
-
-export const WalletContextProvider = ({ children }) => {
+const WalletContextProvider = ({ children }) => {
     const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], []);
 
     return (
         <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
             <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>{children}</WalletModalProvider>
+                {children}
             </WalletProvider>
         </ConnectionProvider>
     );
